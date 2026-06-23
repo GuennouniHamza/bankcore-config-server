8b6a391b4cf143f43d0fb8a1169acbb58e27f554
HamzaGUENNOUNI <120053306+HamzaGUENNOUNI@users.noreply.github.com>
2026-06-24 00:32:57 +0100

    feat: add client and compte service configs, fix encoding

--- src/main/java/ma/bankcore/config_server/ConfigServerApplication.java
+++ src/main/java/ma/bankcore/config_server/ConfigServerApplication.java
@@ -2,6 +2,7 @@ package ma.bankcore.config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
